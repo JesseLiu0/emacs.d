15e8979d953bcc7f96d6dc6125d1e2ceae29c767
Eric Liu <eenliu@gmail.com>
2014-01-22 23:34:09 -0800

    Start up with existing config files

---
+++ init.el
@@ -0,0 +1,92 @@
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; init.el
+;;
+;; Copyright (C) 2013 Eric N. Liu
+;; 
+;; Description: Emacs configuration startup file.
+;; Author: Eric N. Liu (eenliu@gmail.com)
+;; Keywords:
+;; Requirements:
+;; Version: 0.1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Set system environment variables
+;;
+(when (equal system-type 'darwin)
+  (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+  (push "/usr/local/bin" exec-path)
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Library Paths
+;; Note: Keep every emacs library underneath ~/.emacs.d and 
+;;   shun loading them from the system paths. This makes it 
+;;   easier to use this config on multiple systems.
+
+;(push "~/.emacs.d" load-path)
+;; recursively add the sub-directories to load-path
+(let ((default-directory "~/.emacs.d/site-lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; set init file for custom settings
+;;
+(setq custom-file "~/.emacs.d/lisp/init-emacs-custom.el")
+(load custom-file)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Load RCs
+;;
+
+;; elpa package management
+(load "~/.emacs.d/lisp/init-elpa.el")
+
+;; --------------NOT IN USE--------------
+;; Standalone CEDET has to be loaded and built-in CEDET has to be unloaded before CEDET used by others (el-get).
+;;(load "~/.emacs.d/lisp/init-cedet.el") ;; CEDET devel version from bzr
+;; --------------NOT IN USE--------------
+
+;; basic preferences
+(load "~/.emacs.d/lisp/init-emacs-pref.el")  ;; Emacs Preferences
+(load "~/.emacs.d/lisp/init-key-bindings.el")  ;; key bindings
+(load "~/.emacs.d/lisp/init-decor.el")  ;; fonts, color-theme, font zoom, etc.
+(load "~/.emacs.d/lisp/init-server-windows.el") ;; Set up server mode on Windows.
+
+;; buffer, window, file management
+(load "~/.emacs.d/lisp/init-buffer-management.el") ;; iswitchb, ido, uniquify, tab bar for buffer management
+(load "~/.emacs.d/lisp/init-desktop.el") ;; desktop package for saving buffer list and window management
+(load "~/.emacs.d/lisp/init-dired.el") ;; dired, ibuffer, etc. for file management
+
+;; small tools
+(load "~/.emacs.d/lisp/init-swiss-knife.el")
+
+;; big tools
+(load "~/.emacs.d/lisp/init-auto-complete.el") ;; auto-complete
+(load "~/.emacs.d/lisp/init-yasnippet.el")
+(load "~/.emacs.d/lisp/init-pabbrev.el")
+(load "~/.emacs.d/lisp/init-ecb.el")
+
+;; mode configs
+(load "~/.emacs.d/lisp/init-shell-mode.el") ;; shell-mode
+(load "~/.emacs.d/lisp/init-text-mode.el") ;; text-mode
+(load "~/.emacs.d/lisp/init-lisp-mode.el") ;; lisp-mode
+(load "~/.emacs.d/lisp/init-cc-mode.el") ;; cc-mode
+(load "~/.emacs.d/lisp/init-ess-mode.el") ;; ESS-mode
+(load "~/.emacs.d/lisp/init-elpy.el") ;; python support by elpy
+;; (load "~/.emacs.d/lisp/init-matlab-mode.el") ;; matlab-emacs make failed
+(load "~/.emacs.d/lisp/init-css-mode.el") ;; css-mode
+
+
---
+++ lisp/init-cc-mode.el
@@ -0,0 +1,77 @@
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; rc-cc-mode.el
+;;
+;; Copyright (C) 2013 Eric N. Liu
+;; 
+;; Description: cc-mode
+;; Author: Eric N. Liu (eenliu@gmail.com)
+;; Keywords:
+;; Requirements:
+;; Version: 0.1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+(require 'cc-mode)
+
+;; Open .h file in C++ mode instead of C mode.
+;;(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+;; Indentation and Tab offset
+(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+
+;; To cause the *compilation* buffer to automatically scroll to the end of new output
+(setq compilation-scroll-output t)
+
+;; create a face for function calls
+(add-hook 'c-mode-hook
+          (lambda ()
+            (font-lock-add-keywords
+             nil
+             '(("\\<\\(\\sw+\\) ?(" 1 font-lock-function-name-face)) t))) 
+
+(add-hook 'c++-mode-hook
+          (lambda ()
+            (font-lock-add-keywords
+             nil
+             '(("\\<\\(\\sw+\\) ?(" 1 font-lock-function-name-face)) t))) 
+
+;; style I want to use in c++ mode
+(c-add-style "my-style" 
+	     '("stroustrup"
+	       (indent-tabs-mode . nil)        ; use spaces rather than tabs
+	       (c-basic-offset . 4)            ; indent by four spaces
+	       (c-offsets-alist . ((inline-open . 0)  ; custom indentation rules
+				   (brace-list-open . 0)
+				   (statement-case-open . +)))))
+
+(defun my-c++-mode-hook ()
+  (c-set-style "my-style")        ; use my-style defined above
+  (auto-fill-mode)         
+  (c-toggle-auto-hungry-state 1))
+
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
+
+
+;; turn on auto newline and hungry delete
+(add-hook 'c-mode-hook (lambda () (c-toggle-auto-hungry-state 1)))
+(add-hook 'c++-mode-hook (lambda () (c-toggle-auto-hungry-state 1)))
+
+;; Load imenu menubar
+(add-hook 'c-mode-hook (lambda () (imenu-add-menubar-index)))
+(add-hook 'c++-mode-hook (lambda () (imenu-add-menubar-index)))
+
+(require 'auto-complete-clang)
+(define-key c++-mode-map (kbd "C-S-<return>") 'ac-complete-clang)
+
+;; C++ function definitions in a header file are expanded to definitions
+;; in acorresponding source file
+(require 'member-functions)
+(setq mf--source-file-extension "cpp")
+
+
+;; flymake
+(add-hook 'find-file-hook 'flymake-find-file-hook)
