78c14126e69f85b03ea5e2f22afd0313f5db3f02
Eric Liu <eenliu@gmail.com>
2014-02-26 16:07:57 -0800

    Update init-auto-complete.el.

--- lisp/init-auto-complete.el
+++ lisp/init-auto-complete.el
@@ -17,19 +17,19 @@
 
 ;; Default config. For details, see the ac-config-default function code
 ;; in aut-complete-config.el.
-;(require 'auto-complete-config)
-;(ac-config-default)
+(require 'auto-complete-config)
+(ac-config-default)
 
 ;; move data file
 (setq ac-comphist-file "~/.emacs.d/metadata/ac-comphist-file.dat")
 
 ;; set the delay time (seconds) of showing selectin menu
-;(setq ac-auto-show-menu 0.1)
+(setq ac-auto-show-menu 0.2)
 
 ;; Change the height (lines) of completion menu
-;(setq ac-menu-height 20)
+(setq ac-menu-height 20)
 
 ;; Select candidates with C-n/C-p only when completion menu is displayed
-;(setq ac-use-menu-map t)
-;(define-key ac-menu-map "\C-n" 'ac-next)
-;(define-key ac-menu-map "\C-p" 'ac-previous)
+(setq ac-use-menu-map t)
+(define-key ac-menu-map "\C-n" 'ac-next)
+(define-key ac-menu-map "\C-p" 'ac-previous)
