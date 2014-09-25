d06d743340e6257d5328cc5b78b5c3aacb83c97b
N. Liu <eenliu@gmail.com>
2014-09-24 23:11:12 -0700

    Remove GNU ELPA repo.

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -32,7 +32,7 @@
 (setq package-archives '(
 			 ("melpa" . "http://melpa.milkbox.net/packages/")
 ;;			 ("marmalade" . "http://marmalade-repo.org/packages/")
-			 ("gnu" . "http://elpa.gnu.org/packages/")
+;;			 ("gnu" . "http://elpa.gnu.org/packages/")
 			 )) 
 
 (defvar prelude-packages
