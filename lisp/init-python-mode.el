dd8619b5961f65ffb0b995fbf6d53586a535df90
Jesse Liu <jesse@liu.onl>
2016-04-15 17:51:47 -0700

    Update Python lisp for Windows.

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -4,24 +4,20 @@
 ;;
 ;; init-python-mode.el
 ;;
-;; Copyright (C) 2014-2015 N. Liu
+;; Copyright (C) 2014-2016 Jesse Liu
 ;; 
 ;; Description: Setup python programming environment. 
 ;;              Based on built-in python.el and elpy.
-;; Author: Ning Liu (eenliu@gmail.com)
+;; Author: Jesse Liu (jesse@liu.onl)
 ;; Keywords:
 ;; Requirements:
-;; Version: 0.5
+;; Version: 1.0
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; elpy
-;;
+;; Enable elpy
 (elpy-enable)
-;;  (elpy-clean-modeline))
 
 ;; Use jedi as rpc backend
 (setq elpy-rpc-backend "jedi")
@@ -30,18 +26,21 @@
 (when (executable-find "ipython")
  (elpy-use-ipython))
 
+;; Interpreter
 (if (eq system-type 'windows-nt)
     (setq
-     python-shell-interpreter "C:\\apps\\python35\\python.exe"
-     python-shell-interpreter-args "-i C:\\apps\\python35\\Scripts\\ipython-script.py console --matplotlib=qt"
-     elpy-rpc-python-command "C:\\apps\\python35\\python.exe"
+     python-shell-interpreter "C:\\apps\\python\\Scripts\\ipython.exe"
+     python-shell-interpreter-args "-i --matplotlib=qt5"
+     elpy-rpc-python-command "C:\\apps\\python\\python.exe"
      )
   (setq elpy-rpc-python-command "/usr/local/bin/python3")
   (setq python-shell-interpreter-args "--matplotlib")
   )
 
-;; Fine tuning editing behavior
+
+;; Fine tune editing behavior
 (define-key python-mode-map (kbd "RET") 'newline-and-indent)
+(add-hook 'python-mode-hook #'electric-spacing-mode)
 
 (defun python-mode-editing ()
   "My editing preferences for python-mode."
@@ -50,29 +49,18 @@
   )
 (add-hook 'python-mode-hook 'python-mode-editing)
 
-(add-hook 'python-mode-hook #'electric-spacing-mode)
+
+;; Set identation colors not too intrusive
+(set-face-background 'highlight-indentation-face "#282828")
+(set-face-background 'highlight-indentation-current-column-face "#383838")
+
 
 ;; use python-mode for *.pyw and *.wsgi
 (add-to-list 'auto-mode-alist '("\\.pyw\\'" . python-mode))
 (add-to-list 'auto-mode-alist '("\\.wsgi\\'" . python-mode))
 
+
 ;; Use flycheck to replace flymake
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; highlight-indentation is called by elpy
-;; highlight-indentation-mode displays guidelines indentation (space indentation only).
-;; highlight-indentation-current-column-mode displays guidelines for the current-point indentation (space indentation only).
-;;
-
-;; (set-face-background 'highlight-indentation-face "#e3e3d3")
-;; (set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
-(set-face-background 'highlight-indentation-face "#282828")
-(set-face-background 'highlight-indentation-current-column-face "#383838")
-
-;; Turned off highlight-indentation-mode by default as it is distracting.
-;;(add-hook 'elpy-mode-hook (lambda () (highlight-indentation-mode -1)))
